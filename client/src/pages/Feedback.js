5e19c6977c90619d158f1b62b5aeaa582009c109
Shanjei <shanjei@flexagn.com>
2024-04-17 00:32:28 +0530

    feedback form UI added

--- client/src/pages/Feedback.js
+++ client/src/pages/Feedback.js
@@ -5,6 +5,10 @@ import Typography from '@mui/material/Typography';
 import { styled } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import AppbarMain from '../components/Appbar';
+import Paper from "@mui/material/Paper";
+import InputLabel from "@mui/material/InputLabel";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
 
 const Heading = styled(Typography)(({ theme }) => ({
   
@@ -31,6 +35,16 @@ const Item = styled(Box)(({ theme }) => ({
   width: "100%"
 }));
 
+const FormStyledContainer = styled(Paper)(({ theme }) => ({
+  ...theme.typography.body2,
+  padding: theme.spacing(2),
+  textAlign: 'center',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'flex-start',
+  justifyContent: 'left',
+}));
+
 const Feedback = () => {
   return (
     <>
@@ -43,6 +57,61 @@ const Feedback = () => {
             <Typography variant='body2' sx={{textAlign: 'left', paddingTop: 1}}>
               Please fill the form with your feedback.
             </Typography>
+              <FormStyledContainer elevation={3}>
+              <Box sx={{ padding: 3 }}>
+              <Grid container spacing={3}>
+                <Grid item xs={12} sm={2}>
+                  <InputLabel
+                    sx={{
+                      display: "flex",
+                      justifyContent: "center",
+                      fontWeight: 700
+                    }}
+                  >
+                    Name
+                  </InputLabel>
+                </Grid>
+                <Grid item xs={12} sm={10}>
+                  <TextField
+                    required
+                    id="name"
+                    name="name"
+                    label="Name"
+                    fullWidth
+                    size="small"
+                    autoComplete="off"
+                    variant="outlined"
+                  />
+                </Grid>
+                <Grid item xs={12} sm={2}>
+                  <InputLabel
+                    sx={{
+                      display: "flex",
+                      justifyContent: "center",
+                      fontWeight: 700
+                    }}
+                  >
+                    Message
+                  </InputLabel>
+                </Grid>
+                <Grid item xs={12} sm={10}>
+                  <TextField
+                    required
+                    id="outlined-multiline-static"
+                    label="Your message..."
+                    multiline
+                    fullWidth
+                    rows={4}
+                  />
+                </Grid>
+                <Grid item xs={12} sm={2}>
+                  <Button variant="contained">
+                    Submit
+                  </Button>
+                </Grid>
+              </Grid>
+              </Box>
+            </FormStyledContainer>
           </Item>
         </Grid>
       </StyledGrid>
